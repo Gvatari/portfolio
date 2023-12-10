d2ac345db1cbeed7d65427486d79e07622b0d590
Name <lexa7.03.1994@yandex.ru>
2023-12-11 00:16:40 +0300

    header structure

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,16 @@
+import iconsSprite from '../../assets/images/icons-sprite.svg'
+
+type IconPropsType = {
+    iconId: string
+    width?: string
+    height?: string 
+    viewBox?: string
+}
+
+export const Icon = (props: IconPropsType) => {
+    return (
+        <svg width={props.width || "16"} height={props.height || "16"} viewBox={props.viewBox || "0 0 16 16"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
+        </svg>
+    );
+};
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "../icon/Icon";
+
+export const Logo = () => {
+    return (
+        <a href="">
+            <Icon iconId={'notitanic'} width={'65'} height={'48'} viewBox={'0 0 65 48'} />
+        </a>
+    );
+};
+
+
